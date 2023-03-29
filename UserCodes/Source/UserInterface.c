86452c47f7a29728968e0b22fe047e48632e96be
jinyi-s <jinyis12@qq.com>
2023-03-29 12:36:21 +0800

    Finish Basic GUI

--- UserCodes/Source/UserInterface.c
+++ UserCodes/Source/UserInterface.c
@@ -10,24 +10,30 @@ void InitializeGUI(){
     // Initialize Screen Before Using Screen
     ILI9341FillScreen(RGB565_BLACK);
     // Background
-    ILI9341FillRectangle(0, 300, 240, 20, RGB565_ORANGE);
+    ILI9341FillRectangle(0, 0, 240, 20, RGB565_ORANGE);
+    ILI9341DrawString(1, 5, "Status:STOP", Font_07x10, RGB565_CYAN, RGB565_ORANGE);
+    ILI9341DrawString(92, 5, "VREF:3.3V", Font_07x10, RGB565_CYAN, RGB565_ORANGE);
+    ILI9341DrawString(162, 5, "Mode:Wave", Font_07x10, RGB565_CYAN, RGB565_ORANGE);
     // Top Status Bar
-    ILI9341FillRectangle(0, 0, 240, 80, RGB565_ORANGE);
+    ILI9341FillRectangle(0, 260, 240, 60, RGB565_ORANGE);
+    ILI9341DrawString(1, 275, "VerticalGrid: 100mV/div", Font_07x10, RGB565_CYAN, RGB565_ORANGE);
+    ILI9341DrawString(1, 263, "HorizontalGrid: 100uA/div", Font_07x10, RGB565_CYAN, RGB565_ORANGE);
+    ILI9341DrawString(1, 304, "Abstractions' work", Font_11x18, RGB565_DARKCYAN, RGB565_ORANGE); 
     // Buttom Status Bar
-    ILI9341DrawLine(20, 90, 220, 90, RGB565_ORANGE);
-    ILI9341DrawLine(20, 90, 20, 290, RGB565_ORANGE);
-    ILI9341DrawLine(20, 290, 220, 290, RGB565_ORANGE);
-    ILI9341DrawLine(220, 90, 220, 290, RGB565_ORANGE);
-    // Waveform Graphics Paint Box
-    ILI9341DrawLine(20, 130, 220, 130, RGB565_ORANGE);
-    ILI9341DrawLine(20, 170, 220, 170, RGB565_ORANGE);
-    ILI9341DrawLine(20, 210, 220, 210, RGB565_ORANGE);
-    ILI9341DrawLine(20, 250, 220, 250, RGB565_ORANGE);
-    ILI9341DrawLine(20, 290, 220, 290, RGB565_ORANGE);
-    ILI9341DrawLine(60, 90, 60, 290, RGB565_ORANGE);
-    ILI9341DrawLine(100, 90, 100, 290, RGB565_ORANGE);
-    ILI9341DrawLine(140, 90, 140, 290, RGB565_ORANGE);
-    ILI9341DrawLine(180, 90, 180, 290, RGB565_ORANGE);
-    ILI9341DrawLine(220, 90, 220, 290, RGB565_ORANGE);
-    // Grids
+    ILI9341DrawLine(20, 40, 20, 240, RGB565_ORANGE);
+    ILI9341DrawLine(20, 40, 220, 40, RGB565_ORANGE);
+    ILI9341DrawLine(20, 240, 220, 240, RGB565_ORANGE);
+    ILI9341DrawLine(220, 240, 220, 40, RGB565_ORANGE);
+    // Waveform Painter Box
+    ILI9341DrawLine(60, 40, 60, 240, RGB565_ORANGE);
+    ILI9341DrawLine(100, 40, 100, 240, RGB565_ORANGE);
+    ILI9341DrawLine(140, 40, 140, 240, RGB565_ORANGE);
+    ILI9341DrawLine(180, 40, 180, 240, RGB565_ORANGE);
+    // Vertical Grid Lines
+    ILI9341DrawLine(20, 80, 220, 80, RGB565_ORANGE);
+    ILI9341DrawLine(20, 120, 220, 120, RGB565_ORANGE);
+    ILI9341DrawLine(20, 160, 220, 160, RGB565_ORANGE);
+    ILI9341DrawLine(20, 200, 220, 200, RGB565_ORANGE);
+    // Horizontal Grid Lines
+    
 }   
